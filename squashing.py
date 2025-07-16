e3066a683eced779059b70bc33217a9452d4bf29
pound-me <77052958+pound-me@users.noreply.github.com>
2025-07-16 11:00:02 +0800

    Update squashing.py

--- squashing.py
+++ squashing.py
@@ -3,13 +3,11 @@
 import numpy as np
 import math
 
- import crossvalidation  as cr
-
 def squashing(Data):
     row = Data.shape[0]
     col = Data.shape[1]
-     sqData = np.zeros((row, col))
-      for i in range (row):
+    sqData = np.zeros((row, col))
+    for i in range (row):
         for j in range (col):
             sqData[i][j] =( 1-math.cos(Data[i][j]*math.pi))/2
     return sqData
