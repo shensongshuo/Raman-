e84c4ee726a27b75be357906fba2ee85d398f676
pound-me <77052958+pound-me@users.noreply.github.com>
2025-07-16 15:53:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ from i_sigmoid import i_sigmoid
 from IModPoly import IModPoly
 from AsLS import baseline_als
 from LPnorm import LPnorm
-from wavelettransform import waveletlinear
-
-
+from wavelettransform import waveletlinear fft
+from fft import Smfft
+from KalmanFiltering import Kalman
+from ArithmeticAverage import MWA 
+from meadianfiltering import MWM 
+from SGfiltering import SGfilter 
 
 # 设置页面
 st.set_page_config(layout="wide", page_title="拉曼光谱分析系统")
